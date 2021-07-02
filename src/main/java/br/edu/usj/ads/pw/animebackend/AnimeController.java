26fbef79b5130a4f0e2ffb52809b3d4a7326c709
andremachadol <andre-luizmachado@hotmail.com>
2021-07-01 23:37:15 -0300

    Corrigido Deletar

--- src/main/java/br/edu/usj/ads/pw/animebackend/AnimeController.java
+++ src/main/java/br/edu/usj/ads/pw/animebackend/AnimeController.java
@@ -8,10 +8,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+;
 
 @Controller
 @ResponseBody
@@ -41,7 +40,7 @@ public class AnimeController {
     }
 
     @GetMapping(value = "/deletar/{id}")
-    public void getDeletar(@RequestParam Long id) {
+    public void getDeletar(@PathVariable Long id) {
         animeRepository.deleteById(id);
     }
 }
